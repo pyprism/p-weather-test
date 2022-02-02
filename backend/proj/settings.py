38c0ee38fb1ac09d4d5956769202471bd4668a50
Ashutosh Das <git.pyprism@gmail.com>
2022-02-03 00:34:30 +0600

    added csrf excemt

--- backend/proj/settings.py
+++ backend/proj/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'weather.middleware.OpenCORSMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
